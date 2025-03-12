5fdc191a1dce21111438a87fd5e8669f7674f7dd
AyoubElmahjoubi <168941231+AyoubElmahjoubi@users.noreply.github.com>
2025-03-11 19:05:06 -0700

    les test unitaires de partie 2 de tp avec class Notification

--- src/main/java/com/exemple/CompteBancaire.java
+++ src/main/java/com/exemple/CompteBancaire.java
@@ -3,6 +3,18 @@ package com.exemple;
 public class CompteBancaire {
 
     private double solde;
+    private NotificationService notificationService;
+
+    public CompteBancaire() {
+    }
+
+    public CompteBancaire(double soldeInitial, NotificationService notificationService) {
+        if (soldeInitial < 0) {
+            throw new IllegalArgumentException("Le solde initial ne peut pas être négatif.");
+        }
+        this.solde = soldeInitial;
+        this.notificationService = notificationService;
+    }
 
     public CompteBancaire(double soldeInitial) {
         if (soldeInitial < 0) {
@@ -20,6 +32,7 @@ public class CompteBancaire {
             throw new IllegalArgumentException("Le montant du dépôt doit être positif.");
         }
         solde += montant;
+        notificationService.envoyerNotification("Dépôt de " + montant + " effectué.");
     }
 
     public void retirer(double montant) {
@@ -30,5 +43,13 @@ public class CompteBancaire {
             throw new IllegalArgumentException("Fonds insuffisants.");
         }
         solde -= montant;
+        notificationService.envoyerNotification("Retrait de " + montant + " effectué.");
+    }
+
+    public void transfererVers(CompteBancaire autreCompte, double montant){
+        this.retirer(montant);
+        autreCompte.deposer(montant);
+        this.notificationService.envoyerNotification("vous avez transfrer ce montant :"+ montant +" avec succés");
+        autreCompte.notificationService.envoyerNotification("un mantant : "+ montant +" à été ajouter dans votre compte avec succés");
     }
 }
--- src/test/java/com/exemple/CompteBancaireTest.java
+++ src/test/java/com/exemple/CompteBancaireTest.java
@@ -2,8 +2,13 @@ package com.exemple;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 
 class CompteBancaireTest {
 
@@ -13,18 +18,33 @@ class CompteBancaireTest {
     double montantNegatif;
     double retiremontant;
     double retraitmontantSupAuSolde;
+
+    @InjectMocks
     CompteBancaire compteBancaire;
+    @Mock
+    NotificationService notificationService;
+    @Mock
+    NotificationService notificationService_1;
+    @Mock
+    NotificationService notificationService_2;
+
+    CompteBancaire compteBancaire_1;
+    CompteBancaire compteBancaire_2;
+
 
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
         initialSold = 100;
         initialSoldNegatif = -50;
         depotmontant = 50;
         retiremontant = 30;
         retraitmontantSupAuSolde =200;
         montantNegatif = -10;
-        compteBancaire = new CompteBancaire(initialSold);
+        compteBancaire = new CompteBancaire(initialSold , notificationService);
+        compteBancaire_1 = new CompteBancaire(initialSold, notificationService_1);
+        compteBancaire_2 = new CompteBancaire(initialSold, notificationService_2);
     }
 
     @Test
@@ -36,6 +56,7 @@ class CompteBancaireTest {
     void deposer() {
         compteBancaire.deposer(depotmontant);
         assertEquals(150, compteBancaire.getSolde());
+        verify(notificationService).envoyerNotification("Dépôt de " + depotmontant + " effectué.");
     }
 
     @Test
@@ -51,6 +72,7 @@ class CompteBancaireTest {
            compteBancaire.retirer(retraitmontantSupAuSolde);
         });
         assertTrue(thrown.getMessage().contains("Fonds insuffisants"));
+        verify(notificationService, never()).envoyerNotification("Retrait de " + retraitmontantSupAuSolde + " effectué");
     }
 
     @Test
@@ -59,6 +81,7 @@ class CompteBancaireTest {
             compteBancaire.deposer(montantNegatif);
         });
         assertTrue(thrown.getMessage().contains("Le montant du dépôt doit être positif")) ;
+        verify(notificationService, never()).envoyerNotification("Dépôt de " + montantNegatif + " effectué.");
     }
     @Test
     void test_retirer_montant_negatif(){
@@ -66,13 +89,34 @@ class CompteBancaireTest {
            compteBancaire.retirer(montantNegatif);
         });
         assertTrue(thrown.getMessage().contains("Le montant du retrait doit être positif"));
+        verify(notificationService, never()).envoyerNotification("Retrait de " + montantNegatif + " effectué");
     }
 
     @Test
     void test_creation_compte_avec_solde_initial_negatif(){
         var thrown = assertThrows(IllegalArgumentException.class, () -> {
-            compteBancaire = new CompteBancaire(initialSoldNegatif);
+            compteBancaire = new CompteBancaire(initialSoldNegatif , notificationService);
         });
         assertTrue(thrown.getMessage().contains("Le solde initial ne peut pas être négatif"));
     }
+
+    // Pour repondre à la question 4 :
+    @Test
+    void test_deux_depot_successif(){
+        compteBancaire.deposer(depotmontant);
+        compteBancaire.deposer(depotmontant);
+        assertEquals(200, compteBancaire.getSolde());
+        verify(notificationService, times(2)).envoyerNotification("Dépôt de " + depotmontant + " effectué.");
+    }
+
+    // Pour repondre à la question 5 :
+    @Test
+    void test_interaction_complexe_avec_plusieurs_mocks(){
+        compteBancaire_1.transfererVers(compteBancaire_2 ,depotmontant);
+        assertEquals(50, compteBancaire_1.getSolde());
+        assertEquals(150, compteBancaire_2.getSolde());
+        verify(notificationService_1).envoyerNotification("vous avez transfrer ce montant :"+ depotmontant +" avec succés");
+        verify(notificationService_2).envoyerNotification("un mantant : "+ depotmontant +" à été ajouter dans votre compte avec succés");
+    }
+
 }
